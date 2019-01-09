d837df900b6b21c459d0a90d952dfe535968a50d
huangym1 <31759769+huangym1@users.noreply.github.com>
2019-01-09 16:54:37 +0800

    Initial commit

---
+++ blog2/setting.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+#调试模式是否开启
+DEBUG = True
+
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+#session必须要设置key
+SECRET_KEY='A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+#mysql数据库连接信息,这里改为自己的账号
+SQLALCHEMY_DATABASE_URI = "mysql://root:gsta1209@172.19.120.61:3306/sslog"
---
+++ runserver.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# from blog2 import app
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+# import pymysql
+# conn = pymysql.Connect(
+#     host='172.19.120.61',##mysql服务器地址
+#     port=3306,##mysql服务器端口号
+#     user='root',##用户名
+#     passwd='gsta1209',##密码
+#     db='sslog',##数据库名
+#     charset='utf8'##连接编码
+# )
+#
+# cursor = conn.cursor()
+# sql = 'show databases'
+# cursor.execute(sql)
+# rr = cursor.fetchall()
+# if __name__ == '__main__':
+#     for row in rr:
+#         print("%s" % row)
+
